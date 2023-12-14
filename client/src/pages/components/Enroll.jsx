59ab20e3be40aee027084e8443f5cd3f2db86ff4
gerismumo <geraldmumo6@gmail.com>
2023-12-14 22:39:25 +0300

    console enroll form

--- client/src/pages/components/Enroll.jsx
+++ client/src/pages/components/Enroll.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../Footer'
 import Header from '../Header'
 import { coursesDetails, otherCourses } from './Courses'
 
 const Enroll = () => {
-    // console.log(coursesDetails);
+    const[formData, setFormData] = useState(
+        {
+           firstName: '',
+           middleName: '',
+           lastName: '',
+           email: '',
+           phoneNumber: '',
+           gender: '',
+           county: '',
+           birthDate:'',
+           grade: '',
+           course: '',
+           additionalCourse: '',
+        }
+    )
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        console.log('formData',formData);
+    }
   return (
     <>
     <Header />
@@ -19,41 +37,86 @@ const Enroll = () => {
                         <p>Enroll Form</p>
                     </div>
                     <div className="enroll-form">
-                        <form action="">
+                        <form onSubmit={handleSubmit}>
                             <div className="form-parts">
                                 <div className="form-part1">
                                     <label htmlFor="firstName">FirstName:</label>
-                                    <input type="text" name='firstName' />
+                                    <input type="text" 
+                                    name='firstName'
+                                    value={formData.firstName} 
+                                    onChange={(e) => setFormData({...formData, firstName: e.target.value})}
+                                    />
                                     <label htmlFor="middleName">Middle Name:</label>
-                                    <input type="text" name='middleName' />
+                                    <input type="text" 
+                                    name='middleName'
+                                    value={formData.middleName}
+                                    onChange={(e) => setFormData({...formData, middleName: e.target.value})}
+                                     />
                                     <label htmlFor="lastName">Last Name:</label>
-                                    <input type="text" name='lastName' />
+                                    <input type="text"
+                                     name='lastName'
+                                     value={formData.lastName}
+                                     onChange={(e) => setFormData({...formData, lastName:e.target.value})}
+                                      />
                                     <label htmlFor="email">Email:</label>
-                                    <input type="email" name="email" id="email" />
+                                    <input type="email" 
+                                    name="email" 
+                                    id="email" 
+                                    value={formData.email}
+                                    onChange={(e) => setFormData({...formData, email: e.target.value})}
+                                    />
                                     <label htmlFor="phoneNumber">Phone Number:</label>
-                                    <input type="tel" name="phoneNumber" id="phoneNumber" />
+                                    <input type="tel" 
+                                    name="phoneNumber" 
+                                    id="phoneNumber" 
+                                    value={formData.phoneNumber}
+                                    onChange={(e) => setFormData({...formData, phoneNumber: e.target.value})}
+                                    />
                                     <label htmlFor="gender">Gender:</label>
                                     <div className="gender">
                                         <div className="male">
-                                            <input type="radio" name="gender" id="gender"  value='Male'/>
+                                            <input type="radio" 
+                                            name="gender" 
+                                            id="gender"  
+                                            value='Male'
+                                            onChange={(e) => setFormData({...formData, gender: e.target.value})}
+                                            />
                                             <label htmlFor="male">Male</label>
                                         </div>
                                         <div className="female">
-                                            <input type="radio" name="gender" id="gender"  value='Female' />
+                                            <input type="radio" 
+                                            name="gender" 
+                                            id="gender"  
+                                            value='Female' 
+                                            onChange={(e) => setFormData({...formData, gender: e.target.value})}
+                                            />
                                             <label htmlFor="female">Female</label>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="form-part2">
                                     <label htmlFor="county">County:</label>
-                                    <select name="county" id="county">
+                                    <select name="county" 
+                                    id="county" 
+                                    value={formData.county}
+                                    onChange={(e) => setFormData({...formData, county: e.target.value})}
+                                    >
                                         <option value="Mombasa">Mombasa</option>
                                         <option value="Makueni">Makueni</option>
                                     </select>
                                     <label htmlFor="birthDate">Date of Birth:</label>
-                                    <input type="date" name="birthDate" id="birthDate" />
+                                    <input type="date"
+                                     name="birthDate" 
+                                     id="birthDate" 
+                                     value={formData.birthDate}
+                                     onChange={(e) => setFormData({...formData, birthDate: e.target.value})}
+                                     />
                                     <label htmlFor="grade">KCSE Grade:</label>
-                                    <select name="grade" id="grade">
+                                    <select name="grade" 
+                                    id="grade"
+                                    value={formData.grade}
+                                    onChange={(e) => setFormData({...formData, grade: e.target.value})}
+                                    >
                                         <option value="A">A</option>
                                         <option value="B+">B+</option>
                                         <option value="B">B</option>
@@ -67,14 +130,22 @@ const Enroll = () => {
                                         <option value="E">E</option>
                                     </select>
                                     <label htmlFor="course">Course:</label>
-                                    <select name="course" id="course">
+                                    <select name="course" 
+                                    id="course"
+                                    value={formData.course}
+                                    onChange={(e) => setFormData({...formData, course: e.target.value})}
+                                    >
                                         <option value='None'>None</option>
                                         {coursesDetails.map((course) => (
                                             <option key={course.id} value={course.name}>{course.name}</option>
                                         )) }
                                     </select>
                                     <label htmlFor="additionalCourses">Additional Courses:</label>
-                                        <select name="additionalCourses" id="additionalCourses">
+                                        <select name="additionalCourses" 
+                                        id="additionalCourses"
+                                        value={formData.additionalCourse}
+                                        onChange={(e) => setFormData({...formData, additionalCourse: e.target.value})}
+                                        >
                                             <option value='None'>None</option>
                                         {otherCourses.map((course) => (
                                             <option key={course.id} value={course.name}>{course.name}</option>
