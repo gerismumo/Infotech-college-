734d399322cd08230a49858dfad8543a9f33df32
gerismumo <geraldmumo6@gmail.com>
2023-12-15 00:41:57 +0300

    database connection

--- server/app.js
+++ server/app.js
@@ -7,7 +7,14 @@ dotenv.config({path: './database/.env'});
 app.use(express.json())
 app.use(cors());
 
+const database = require('./database/db')
+
 const port = process.env.PORT;
-app.listen( port, () => {
-    console.log(`Server listening on Port ${port}`);
-})
+
+const startServer = () => {
+    database.createConnection();
+    app.listen( port, () => {
+        console.log(`Server listening on Port ${port}`);
+    })
+}
+startServer();
