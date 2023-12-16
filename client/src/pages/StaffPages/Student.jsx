e402833ff5bd687b1fe80c24dfefb03593c26f56
gerismumo <geraldmumo6@gmail.com>
2023-12-16 06:30:43 +0300

    student edit

--- client/src/pages/StaffPages/Student.jsx
+++ client/src/pages/StaffPages/Student.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { coursesDetails, otherCourses } from '../components/Courses';
 
 const Student = () => {
     const[studentList, setStudentList] = useState([]);
+    const[openEdit, setOpenEdit] = useState(false);
+    const[editId, setEditId] = useState(null);
 
     const approved_student_api = `${process.env.REACT_APP_API_URL}/api/approvedStudents`;
 
@@ -35,8 +39,66 @@ const Student = () => {
         }
      }
 
+     const [countiesList, setCountiesList] = useState([]);
+    const counties_api = `${process.env.REACT_APP_API_URL}/api/counties`;
+
+    useEffect(() => {
+        const fetchCounties = async() => {
+            try {
+                const response = await axios.get(counties_api);
+                const success = response.data.success;
+                if(success) {
+                    setCountiesList(response.data.data);
+                }
+            }catch(error) {
+                console.log(error.message);
+            }
+        }
+        fetchCounties();
+    },[counties_api]);
+
+    const [editForm, setEditForm] = useState(null);
+     const handleEdit = (id) => {
+        setEditId(id);
+        const editUser = studentList.find(student => student.id === id);
+        setEditForm(editUser);
+        setOpenEdit(true);
+     }
+
+     const handleSubmitEdit = async(e) => {
+        e.preventDefault();
+
+        // console.log('edit form',editForm);
+
+        for(const key in editForm) {
+            if(editForm[key] === '') {
+                toast.error(`Please fill all the values`);
+                return;
+            }
+        }
+        const edit_user_api = `${process.env.REACT_APP_API_URL}/api/updateUser/${editId}`;
+        try {
+            const response = await axios.put(edit_user_api, editForm);
+            const success = response.data.success;
+            if(success) {
+                toast.success(response.data.message)
+                setTimeout(() => {
+                    setOpenEdit(false);
+                }, 1000);
+                
+            } else {
+                toast.error(response.data.message)
+            }
+            // console.log(response);
+        } catch (error) {
+            toast.error(error.message);
+        }
+     }
+      
+
   return (
     <div className="enrollment-page">
+        <ToastContainer />
         <div className="enroll-content">
             <div className="enroll-table">
                 <table>
@@ -56,22 +118,165 @@ const Student = () => {
                     </thead>
                     <tbody>
                         {studentList.map((student) => (
-                            <tr key={student.id}>
-                                <td>{student.first_name}</td>
-                                <td>{student.middle_name}</td>
-                                <td>{student.last_name}</td>
-                                <td>{student.student_email}</td>
-                                <td>{student.phone_number}</td>
-                                <td>{student.gender}</td>
-                                <td>{student.county}</td>
-                                <td>{student.birth_date}</td>
-                                <td>{student.grade}</td>
-                                <td>{student.course}</td>
-                                <td>{student.additional_course}</td>
-                                <td>{student.approved === 0 ? 'No' : 'Yes'}</td>
-                                <td><button>Edit</button></td>
-                                <td><button onClick={() => handleDelete(student.id)}>Delete</button></td>
-                            </tr>
+                            <React.Fragment key={student.id}>
+                                <tr key={student.id}>
+                                    <td>{student.first_name}</td>
+                                    <td>{student.middle_name}</td>
+                                    <td>{student.last_name}</td>
+                                    <td>{student.student_email}</td>
+                                    <td>{student.phone_number}</td>
+                                    <td>{student.gender}</td>
+                                    <td>{student.county}</td>
+                                    <td>{student.birth_date}</td>
+                                    <td>{student.grade}</td>
+                                    <td>{student.course}</td>
+                                    <td>{student.additional_course}</td>
+                                    <td>{student.approved === 0 ? 'No' : 'Yes'}</td>
+                                    <td><button onClick={() => handleEdit(student.id)} >Edit</button></td>
+                                    <td><button onClick={() => handleDelete(student.id)}>Delete</button></td>
+                                </tr>
+                                {openEdit && editId === student.id && (
+                                    <tr>
+                                        <td colSpan='15'>
+                                        <>
+                                        <div className="edit-form">
+                                        <div className="enroll-form">
+                                                <div className="close-button">
+                                                    <button onClick={() => setOpenEdit(false)}>Close</button>
+                                                </div>
+                                                <form onSubmit={(e) => handleSubmitEdit(e)}>
+                                                    <div className="form-parts">
+                                                        <div className="form-part1">
+                                                            <label htmlFor="firstName">FirstName:</label>
+                                                            <input type="text" 
+                                                            name='firstName'
+                                                            value={editForm.first_name} 
+                                                            onChange={(e) => setEditForm({...editForm, first_name: e.target.value})}
+                                                            />
+                                                            <label htmlFor="middleName">Middle Name:</label>
+                                                            <input type="text" 
+                                                            name='middleName'
+                                                            value={editForm.middle_name}
+                                                            onChange={(e) => setEditForm({...editForm, middle_name: e.target.value})}
+                                                            />
+                                                            <label htmlFor="lastName">Last Name:</label>
+                                                            <input type="text"
+                                                            name='lastName'
+                                                            value={editForm.last_name}
+                                                            onChange={(e) => setEditForm({...editForm, last_name:e.target.value})}
+                                                            />
+                                                            <label htmlFor="email">Email:</label>
+                                                            <input type="email" 
+                                                            name="email" 
+                                                            id="email" 
+                                                            value={editForm.student_email}
+                                                            onChange={(e) => setEditForm({...editForm, student_email: e.target.value})}
+                                                            />
+                                                            <label htmlFor="phoneNumber">Phone Number:</label>
+                                                            <input type="tel" 
+                                                            name="phoneNumber" 
+                                                            id="phoneNumber" 
+                                                            value={editForm.phone_number}
+                                                            onChange={(e) => setEditForm({...editForm, phone_number: e.target.value})}
+                                                            />
+                                                            <label htmlFor="gender">Gender:</label>
+                                                            <div className="gender">
+                                                                <div className="male">
+                                                                    <input type="radio" 
+                                                                    name="gender" 
+                                                                    id="gender"  
+                                                                    value='Male'
+                                                                    checked={editForm.gender === 'Male'}
+                                                                    onChange={(e) => setEditForm({...editForm, gender: e.target.value})}
+                                                                    />
+                                                                    <label htmlFor="male">Male</label>
+                                                                </div>
+                                                                <div className="female">
+                                                                    <input type="radio" 
+                                                                    name="gender" 
+                                                                    id="gender"  
+                                                                    value='Female' 
+                                                                    checked={editForm.gender === 'Female'}
+                                                                    onChange={(e) => setEditForm({...editForm, gender: e.target.value})}
+                                                                    />
+                                                                    <label htmlFor="female">Female</label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="form-part2">
+                                                            <label htmlFor="county">County:</label>
+                                                            <select name="county" 
+                                                            id="county" 
+                                                            value={editForm.county}
+                                                            onChange={(e) => setEditForm({...editForm, county: e.target.value})}
+                                                            >
+                                                                {countiesList.map((county) => (
+                                                                    <option key={county.code} value={county.name}>{county.name}</option>
+                                                                ))}
+                                                            </select>
+                                                            <label htmlFor="birthDate">Date of Birth:</label>
+                                                            <input type="date"
+                                                            name="birthDate" 
+                                                            id="birthDate" 
+                                                            value={editForm.birth_date}
+                                                            onChange={(e) => setEditForm({...editForm, birth_date: e.target.value})}
+                                                            />
+                                                            <label htmlFor="grade">KCSE Grade:</label>
+                                                            <select name="grade" 
+                                                            id="grade"
+                                                            value={editForm.grade}
+                                                            onChange={(e) => setEditForm({...editForm, grade: e.target.value})}
+                                                            >
+                                                                <option value="A">A</option>
+                                                                <option value="B+">B+</option>
+                                                                <option value="B">B</option>
+                                                                <option value="B-">B-</option>
+                                                                <option value="C+">C+</option>
+                                                                <option value="C">C</option>
+                                                                <option value="C-">C-</option>
+                                                                <option value="D+">D+</option>
+                                                                <option value="D">D</option>
+                                                                <option value="D-">D-</option>
+                                                                <option value="E">E</option>
+                                                            </select>
+                                                            <label htmlFor="course">Course:</label>
+                                                            <select name="course" 
+                                                            id="course"
+                                                            value={editForm.course}
+                                                            onChange={(e) => setEditForm({...editForm, course: e.target.value})}
+                                                            >
+                                                                <option value='None'>None</option>
+                                                                {coursesDetails.map((course) => (
+                                                                    <option key={course.id} value={course.name}>{course.name}</option>
+                                                                )) }
+                                                            </select>
+                                                            <label htmlFor="additionalCourses">Additional Courses:</label>
+                                                                <select name="additionalCourses" 
+                                                                id="additionalCourses"
+                                                                value={editForm.additional_course}
+                                                                onChange={(e) => setEditForm({...editForm, additional_course: e.target.value})}
+                                                                >
+                                                                    <option value='None'>None</option>
+                                                                {otherCourses.map((course) => (
+                                                                    <option key={course.id} value={course.name}>{course.name}</option>
+                                                                )) }
+                                                                </select>
+                                                        </div>
+                                                    </div>
+                                                    <div className="button">
+                                                        <button type='submit'>Submit</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                            
+                                        </>
+                                        </td>
+                                    </tr>
+                                    
+                                )}
+                            </React.Fragment>
+                            
                         ))}
                     </tbody>
                 </table>
