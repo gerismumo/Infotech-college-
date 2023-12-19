0ed4420aaa5242ba0a708832f2abceef22ca6017
gerismumo <geraldmumo6@gmail.com>
2023-12-19 19:28:46 +0300

    footer update css

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -9,28 +9,30 @@ const Footer = () => {
         <div className="footer-details">
             <div className="address-details">
                 <div className="address">
-                    <h2>INSTITUTE OF PROFESSIONAL STUDIES</h2>
+                    <div className="footer-logo">
+                        <img src="../../images/infotectlogo.jpg" alt="" />
+                    </div>
                     <p>Nairobi</p>
                     <p>P.O BOX 7964-0610 Nairobi</p>
                     <p>Tel: 0723 487 788 & 0714 661 166</p>
                     <p>Email: infotechinstitude@gmail.com</p>
                 </div>
-                <div className="social-icons">
-                    <FontAwesomeIcon icon={faTwitter} className='iconFooter'/>
-                    <FontAwesomeIcon icon={faFacebook} className='iconFooter'/>
-                    <FontAwesomeIcon icon={faWhatsapp} className='iconFooter'/>
+                <div className="links-details">
+                    <div className="link-title">
+                        <h2>Links</h2>
+                    </div>
+                    <div className="links">
+                        <Link to="">Home</Link>
+                        <Link to="">About</Link>
+                        <Link to="">Courses</Link>
+                        <Link to="">Contact</Link>
+                    </div>
                 </div>
             </div>
-            <div className="links-details">
-                <div className="link-title">
-                    <h2>Links</h2>
-                </div>
-                <div className="links">
-                    <Link to="">Home</Link>
-                    <Link to="">About</Link>
-                    <Link to="">Courses</Link>
-                    <Link to="">Contact</Link>
-                </div>
+            <div className="social-icons">
+                <FontAwesomeIcon icon={faTwitter} className='iconFooter'/>
+                <FontAwesomeIcon icon={faFacebook} className='iconFooter'/>
+                <FontAwesomeIcon icon={faWhatsapp} className='iconFooter'/>
             </div>
         </div>
         <div className="bottom-links">
@@ -38,7 +40,7 @@ const Footer = () => {
                 <p>&copy; 2023 info Tech. All rights reserved.</p>
             </div>
             <div className="developer-details">
-               <p>Designed by <span>Gerald</span></p> 
+               <p>Designed by <span>Gerald</span> & <span>Winnie</span></p> 
             </div>
         </div>
         
