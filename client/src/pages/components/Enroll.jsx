5d318a6764721c467942bb2f9aeb8caa4b013350
gerismumo <geraldmumo6@gmail.com>
2023-12-24 17:05:45 +0300

    addeded

--- client/src/pages/components/Enroll.jsx
+++ client/src/pages/components/Enroll.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from '../Footer';
