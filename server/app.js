776590894f09deed06c97ffadf507e9fbb0a49b9
gerismumo <geraldmumo6@gmail.com>
2023-12-26 05:46:18 +0300

    server edit files

--- server/app.js
+++ server/app.js
@@ -4,8 +4,14 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config({path: './database/.env'});
 
+
+const corsOptions = {
+    origin: 'http://infotech.tsidn.org',
+    methods: ['POST', 'GET', 'PUT', 'DELETE'],
+}
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
+
 
 const createConnection = require('./database/db');
 
